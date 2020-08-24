ab6e5f72993d1aae9db54021d7389cb3cd55259a
zach <ztbochanski@gmail.com>
2020-08-23 21:29:23 -0700

    updated pages with a preview

--- ecommerce-demo/taggart-burberry/src/App.js
+++ ecommerce-demo/taggart-burberry/src/App.js
@@ -3,19 +3,14 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/hompage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats page</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.components.jsx";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
